3f8ed3d02571212cf5934fe6fb4653ced53d2d7a
anish-9387 <anishmall3879@gmail.com>
2025-03-03 17:15:48 +0000

    Fixed the error

--- Week 6/TestFinallyBlock1.java
+++ Week 6/TestFinallyBlock1.java
@@ -1,18 +1,18 @@
-// Program where exception is not thrown
+// // Program where exception is not thrown
 
-public class TestFinallyBlock1{
-    public static void main(String[] args) {
-        try{
-            int data=25/5;
-            System.out.println(data);
-        }
-        catch(NullPointerException e){
-            System.out.println(e);
-        }
-        finally{
-            System.out.println("Finally block is always executed");
-        }
+// public class TestFinallyBlock1{
+//     public static void main(String[] args) {
+//         try{
+//             int data=25/5;
+//             System.out.println(data);
+//         }
+//         catch(NullPointerException e){
+//             System.out.println(e);
+//         }
+//         finally{
+//             System.out.println("Finally block is always executed");
+//         }
 
-        System.out.println("Rest of the code");
-    }
-}
+//         System.out.println("Rest of the code");
+//     }
+// }
